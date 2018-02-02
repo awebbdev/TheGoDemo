56caf34306813b1500e6977babe294fa7b919cf8
thebigbadox <adamwebb13@gmail.com>
2018-02-02 07:03:12 -0600

    Completing GoWeb app

--- GoWeb/hello_http.go
+++ GoWeb/hello_http.go
@@ -1,1 +1,15 @@
-package GoWeb
+package main
+
+import (
+	"net/http"
+	"fmt"
+)
+
+func handler(writer http.ResponseWriter, request *http.Request){
+	fmt.Fprintf(writer, "Hello World, %s!", request.URL.Path[1:])
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8080", nil)
+}
